6f076ffbe89306e673a87148f744bafcf4b667cd
Mark Sommers <42284859+marks0mmers@users.noreply.github.com>
2019-02-20 11:49:07 -0500

    Update pi_a_driver.py

--- pi_a_driver.py
+++ pi_a_driver.py
@@ -17,17 +17,21 @@ LIGHT_MAX = 1200
 POTENT_MAX = 1023
 client = mqtt.Client('pi_a_client')
 
-#setup function for some setup---custom function
-def setup():
-    #set the gpio modes to BCM numbering
-    GPIO.setmode(GPIO.BCM)
-    # set up the SPI interface pins
-    GPIO.setup(MOSI, GPIO.OUT)
-    GPIO.setup(MISO, GPIO.IN)
-    GPIO.setup(CLK, GPIO.OUT)
-    GPIO.setup(CS, GPIO.OUT)
-    client.connect(host="174.99.21.72", port="1883", keepalive="60", bind_address="")
-    pass
+def on_log(client, userdata, level, buf):
+    print(buf)
+
+def on_disconnect(client, userdata, flags, rc=0):
+    ret = client.publish("Status/RasberryPiA", "offline", qos = 2, retain = 2)
+    print("Publish ", ret) 
+    print("Disconnected OK")
+    
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        # Set flag that a client is connected
+        client.connected_flag = True
+        print("Connected OK")
+    else:
+        print("Bad connection! Returned code = ", rc)
 
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
@@ -35,22 +39,49 @@ def on_message(client, userdata, message):
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
 
+#setup function for some setup---custom function
+def setup():
+    #set the gpio modes to BCM numbering
+    GPIO.setmode(GPIO.BCM)
+    
+    # set up the SPI interface pins
+    GPIO.setup(MOSI, GPIO.OUT)
+    GPIO.setup(MISO, GPIO.IN)
+    GPIO.setup(CLK, GPIO.OUT)
+    GPIO.setup(CS, GPIO.OUT)
+    
+    # Bind call backs
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_log = on_log
+    client.on_message = on_message
+    
+    # Set last will message
+    lwm = "offline"
+    lastWillTopic = "Status/RaspberryPiA"
+    client.will_set(lastWillTopic, lwm, qos = 2, retain = True)
+    
+    client.connect(host="174.99.21.72", port="1883")
+    pass
+
 def main():
     print('Reading MCP3008 values, press Ctrl-C to quit...')
-    client.on_message = on_message
     client.loop_start()
     # Print nice channel column headers.
     print('| {0:>4} | {1:>4} |'.format(*range(2)))
     print('-' * 57)
     # Main program loop.
-    while True:
+    while not client.connected_flag:
         # Read all the ADC channel values in a list.
         values = [0]*2
         # The read_adc function will get the value of the specified channel (0-7).
         values[0] = mcp.read_adc(0) * 1.0 / LIGHT_MAX
         values[1] = mcp.read_adc(1) * 1.0 / POTENT_MAX
+
         client.publish("lightSensor", values[0])
         client.publish("threshold", values[1])
+        client.subscribe("lightSensor", 2)
+        client.subscribe("threshold", 2)
 
 
         # Print the ADC values.
