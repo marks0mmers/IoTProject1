d77707852f5f2872add3d89e192bb33daa003b80
Mark Sommers <42284859+marks0mmers@users.noreply.github.com>
2019-02-20 16:09:59 -0500

    Update pi_a_driver.py

--- pi_a_driver.py
+++ pi_a_driver.py
@@ -38,7 +38,7 @@ def on_connect(client, userdata, flags, rc):
         client.loop_stop()
 
 def on_message(client, userdata, message):
-    #print("message received " ,str(message.payload.decode("utf-8")))
+    print("message received " ,str(message.payload.decode("utf-8")))
     #print("message topic=",message.topic)
     #print("message qos=",message.qos)
     #print("message retain flag=",message.retain)
