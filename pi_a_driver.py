ab5d816ed93d9056b3fd3d49882c23285bd5c23c
Mark Sommers <42284859+marks0mmers@users.noreply.github.com>
2019-02-20 12:00:32 -0500

    Update pi_a_driver.py

--- pi_a_driver.py
+++ pi_a_driver.py
@@ -61,7 +61,7 @@ def setup():
     lastWillTopic = "Status/RaspberryPiA"
     client.will_set(lastWillTopic, lwm, qos = 2, retain = True)
     
-    client.connect(host="152.7.255.201", port="1883")
+    client.connect(host="10.153.3.190", port="1883")
     pass
 
 def main():
